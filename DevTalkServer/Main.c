f82527ee182b18c1ad3d6f57e1a29aeb704adc19
이길복 <pst1302@gmail.com>
2015-08-18 21:18:32 +0900

    멀티플렉싱 서버 오랜만에 복기 할 겸 예제 작성

--- DevTalkServer/Main.c
+++ DevTalkServer/Main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<WinSock2.h>
+
+#define BUF_SIZE 1024
 
 void ErrorHandling(char* message);
 
@@ -10,11 +13,16 @@ int main(int argc, char* argv[])
 	// Socket 관련 변수들
 	WSADATA wsaData;
 	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAddr, clntAddr;
+	SOCKADDR_IN servAdr, clntAdr;
 
+	// select 함수 관련 변수 
+	TIMEVAL timeout;
+	fd_set reads, cpyReads;
+	
 	// 일반 변수
-	int szClntAddr;
-	char message[] = "Hello World!";
+	int adrSz;
+	int strLen, fdNum, i;
+	char buf[BUF_SIZE];
 
 	// main argc 유효성 검사
 	if (argc != 2) {
@@ -35,15 +43,15 @@ int main(int argc, char* argv[])
 	}
 	
 	// 서버 소켓 설정
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAddr.sin_port = htons(atoi(argv[1]));
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
 
 	fputs("서버 소켓 바인딩 중....\n", stdout);
 
 	// 소켓 바인드
-	if (bind(hServSock, (SOCKADDR*) &servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
+	if (bind(hServSock, (SOCKADDR*) &servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
 		ErrorHandling("bind() error");
 	}
 
@@ -52,7 +60,7 @@ int main(int argc, char* argv[])
 
 	int size = sizeof(strAddrBuf);
 
-	WSAAddressToString((SOCKADDR*)&servAddr, sizeof(servAddr), NULL, strAddrBuf, &size);
+	WSAAddressToString((SOCKADDR*)&servAdr, sizeof(servAdr), NULL, strAddrBuf, &size);
 
 	fputs("서버 소켓 생성....\n", stdout);
 	fputs("서버 ip주소 : ", stdout);
@@ -67,14 +75,50 @@ int main(int argc, char* argv[])
 	if (listen(hServSock, 5) == SOCKET_ERROR) {
 		ErrorHandling("listen() error!");
 	}
+	
+	FD_ZERO(&reads);
+	FD_SET(hServSock, &reads);
 
-	// Client accept
-	szClntAddr = sizeof(clntAddr);
-	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
-	if (hClntSock == INVALID_SOCKET)
-		ErrorHandling("accept() error");
+	while (1) {
+		cpyReads = reads;
 
-	send(hClntSock, message, sizeof(message), 0);
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 5000;
+
+		if ((fdNum = select(0, &cpyReads, 0, 0, &timeout)) == SOCKET_ERROR)
+			break;
+
+		if (fdNum == 0)
+			continue;
+
+		for (i = 0; i < reads.fd_count; i++) 
+		{
+			// Connection Request
+			if (FD_ISSET(reads.fd_array[i], &cpyReads)){
+				if (reads.fd_array[i] == hServSock) {
+					adrSz = sizeof(clntAdr);
+					hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &adrSz);
+					FD_SET(hClntSock, &reads);
+					printf("connected client: %d \n", hClntSock);
+				}
+				else
+				{
+					strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
+					// 메세지 전송 끝
+					if (strLen == 0) {
+						FD_CLR(reads.fd_array[i], &reads);
+						closesocket(cpyReads.fd_array[i]);
+						printf("close client: %d \n", cpyReads.fd_array[i]);
+					}
+					else {
+						send(reads.fd_array[i], buf, strLen, 0);	// echo
+					}
+				}
+			}
+		}
+	}
+
+
 	closesocket(hClntSock);
 	closesocket(hServSock);
 	WSACleanup();
