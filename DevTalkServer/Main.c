71f91cc0a0f57eb0525f6bb685fb467b07d57f34
pst1302 <pst1302@gmail.com>
2015-08-24 08:41:25 +0900

    IOCP echo functions

--- DevTalkServer/Main.c
+++ DevTalkServer/Main.c
@@ -1,76 +1,93 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<WinSock2.h>
+#include<Windows.h>
 #include<process.h>
-#include<windows.h>
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS = 10;
+#define BUF_SIZE 1024
 
+void CALLBACK ReadCompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void CALLBACK WriteCompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
 void ErrorHandling(char* msg);
+
+typedef struct {
+	SOCKET hClntSock;
+	char buf[BUF_SIZE];
+	WSABUF wsaBuf;
+}PER_IO_DATA, *LPPER_IO_DATA;
 
 int main(int argc, char* argv[]) {
 
 	// 소켓 관련 변수
 	WSADATA wsaData;
-	SOCKET hSocket;
-	SOCKADDR_IN sendAdr;
+	SOCKET hLisnSock, hRecvSock;
+	SOCKADDR_IN lisnAdr, recvAdr;
+	
+	// Overlapped 변수
+	LPWSAOVERLAPPED lpOvLp;
+	DWORD recvBytes;
+	LPPER_IO_DATA hbInfo;
+	int mode = 1, recvAdrSz, flagInfo = 0;
 
-	// 메세지 관련 변수
-	char msg[] = "Network is Computer!";
-	int sendBytes = 0;
-
-	// Overlapped 관련 변수
-	WSABUF dataBuf;
-	WSAEVENT evObj;
-	WSAOVERLAPPED overlapped;
-
-	if (argc != 3) {
-		printf("Usage : %s <ip> <port>\n", argv[0]);
+	if (argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
 		getchar();
 		exit(1);
 	}
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		ErrorHandling("WSAStartup error!");
-	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup error");
 
-	// WSA소켓 생성, overlapped 설정
-	hSocket = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	memset(&sendAdr, 0, sizeof(sendAdr));
-	sendAdr.sin_family = AF_INET;
-	sendAdr.sin_addr.s_addr = inet_addr(argv[1]);
-	sendAdr.sin_port = htons(atoi(argv[2]));
+	hLisnSock = WSASocketW(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	ioctlsocket(hLisnSock, FIONBIO, &mode);	// non-blocking
 
-	// 소켓 연결
-	if (connect(hSocket, (SOCKADDR*)&sendAdr, sizeof(sendAdr))) {
-		ErrorHandling("connect error");
-	}
+	memset(&lisnAdr, 0, sizeof(lisnAdr));
+	lisnAdr.sin_family = AF_INET;
+	lisnAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	lisnAdr.sin_port = htons(atoi(argv[1]));
 
-	// Event 생성 , 메세지 설정
-	evObj = WSACreateEvent();
-	memset(&overlapped, 0, sizeof(overlapped));
-	dataBuf.len = strlen(msg) - 1;
-	dataBuf.buf = msg;
+	if (bind(hLisnSock, (SOCKADDR*)&lisnAdr, sizeof(lisnAdr)) == SOCKET_ERROR)
+		ErrorHandling("bind error");
 
-	// 메세지 전달
-	if (WSASend(hSocket, &dataBuf, 1, &sendBytes, 0, &overlapped, NULL) == SOCKET_ERROR) {
-		// 바로 전달되는 양이 아닌경우 WSA_IO_PENDING setting 되고 백그라운드에서 계속 데이타 전송
-		if (WSAGetLastError() == WSA_IO_PENDING) {
-			puts("Background data send");
-			WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
-			WSAGetOverlappedResult(hSocket, &overlapped, &sendBytes, FALSE, NULL);
+	if (listen(hLisnSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen error");
+
+	// accept
+	recvAdrSz = sizeof(recvAdr);
+	while (1) {
+		SleepEx(100, TRUE);
+		hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAdr, &recvAdrSz);
+		if (hRecvSock == INVALID_SOCKET){
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			else
+				ErrorHandling("accept() error");
 		}
-		else {
-			ErrorHandling("WSASend error!");
-		}
+		puts("Client connected........");
 
-		printf("Send data size: %d \n", sendBytes);
-		WSACloseEvent(evObj);
-		closesocket(hSocket);
-		WSACleanup();
-		return 0;
+		// overlap 변수 메모리 할당 및 셋팅
+		lpOvLp = (LPWSAOVERLAPPED)malloc(sizeof(WSAOVERLAPPED));
+		memset(lpOvLp, 0, sizeof(WSAOVERLAPPED));
+
+		// LPPER_IO_DATA 메모리 할당 및 셋팅
+		hbInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+		hbInfo->hClntSock = (DWORD)hRecvSock;
+		(hbInfo->wsaBuf).buf = hbInfo->buf;
+		(hbInfo->wsaBuf).len = BUF_SIZE;
+
+		// Receive
+		lpOvLp->hEvent = (HANDLE)hbInfo;
+		WSARecv(hRecvSock, &(hbInfo->wsaBuf), 1, &recvBytes, &flagInfo, lpOvLp, ReadCompRoutine);
 	}
+
+	closesocket(hRecvSock);
+	closesocket(hLisnSock);
+	WSACleanup();
+
+	getchar();
+	return 0;
+	
 }
 
 void ErrorHandling(char* msg) {
@@ -79,3 +96,32 @@ void ErrorHandling(char* msg) {
 	exit(1);
 }
 
+
+void CALLBACK ReadCompRoutine(DWORD dwError, DWORD szRecvBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags) {
+	
+	LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
+	SOCKET hSock = hbInfo->hClntSock;
+	LPWSABUF bufInfo = &(hbInfo->wsaBuf);
+	DWORD sentBytes;
+
+	if (szRecvBytes == 0)
+	{
+		closesocket(hSock);
+		free(lpOverlapped->hEvent); free(lpOverlapped);
+		puts("Client disconnected....");
+	}
+	else {		// echo
+		bufInfo->len = szRecvBytes;
+		WSASend(hSock, bufInfo, 1, &sentBytes, 0, lpOverlapped, WriteCompRoutine);
+	}
+}
+void CALLBACK WriteCompRoutine(DWORD dwError, DWORD szSendBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags) {
+
+	LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
+	SOCKET hSock = hbInfo->hClntSock;
+	LPWSABUF bufInfo = &(hbInfo->wsaBuf);
+	DWORD recvBytes;
+	int flagInfo = 0;
+
+	WSARecv(hSock, bufInfo, 1, &recvBytes, &flagInfo, lpOverlapped, ReadCompRoutine);
+}
