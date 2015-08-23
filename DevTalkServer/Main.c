30b88040d2ccf8d2e1b128bab051cb71ea398348
pst1302 <pst1302@gmail.com>
2015-08-23 22:07:31 +0900

    OverlappedIO

--- DevTalkServer/Main.c
+++ DevTalkServer/Main.c
@@ -8,61 +8,67 @@
 void ErrorHandling(char* msg);
 
 int main(int argc, char* argv[]) {
-	
+
+	// 소켓 관련 변수
 	WSADATA wsaData;
 	SOCKET hSocket;
 	SOCKADDR_IN sendAdr;
 
-	WSABUF dataBuf;
-	char msg[] = "Network is Computer";
+	// 메세지 관련 변수
+	char msg[] = "Network is Computer!";
 	int sendBytes = 0;
 
+	// Overlapped 관련 변수
+	WSABUF dataBuf;
 	WSAEVENT evObj;
 	WSAOVERLAPPED overlapped;
 
 	if (argc != 3) {
-		printf("Usage: %s <IP> <port>\n", argv[0]);
+		printf("Usage : %s <ip> <port>\n", argv[0);
+		getchar();
 		exit(1);
 	}
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup() error!");
-	
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		ErrorHandling("WSAStartup error!");
+	}
+
+	// WSA소켓 생성, overlapped 설정
 	hSocket = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	memset(&sendAdr, 0, sizeof(sendAdr));
 	sendAdr.sin_family = AF_INET;
 	sendAdr.sin_addr.s_addr = inet_addr(argv[1]);
-	sendAdr.sin_port = htons(atoi(argv[2]));
+	sendAdr.sin_port = htos(atoi(argv[2]));
 
-	if (connect(hSocket, (SOCKADDR*)&sendAdr, sizeof(sendAdr)) == SOCKET_ERROR)
-		ErrorHandling("connect() error!");
+	// 소켓 연결
+	if (connect(hSocket, &sendAdr, sizeof(sendAdr))) {
+		ErrorHandling("connect error");
+	}
 
-	// Overlapped 이벤트에 연동
+	// Event 생성 , 메세지 설정
 	evObj = WSACreateEvent();
 	memset(&overlapped, 0, sizeof(overlapped));
-	overlapped.hEvent = evObj;
-	dataBuf.len = strlen(msg) + 1;
+	dataBuf.len = strlen(msg) - 1;
 	dataBuf.buf = msg;
 
-	// Sending
-	if (WSASend(hSocket, &dataBuf, 1, &sendBytes, 0, &overlapped, NULL) == SOCKET_ERROR)  {
-		// 에러처리
+	// 메세지 전달
+	if (WSASend(hSocket, &dataBuf, 1, &sendBytes, 0, &overlapped, NULL) == SOCKET_ERROR) {
+		// 바로 전달되는 양이 아닌경우 WSA_IO_PENDING setting 되고 백그라운드에서 계속 데이타 전송
 		if (WSAGetLastError() == WSA_IO_PENDING) {
 			puts("Background data send");
 			WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
 			WSAGetOverlappedResult(hSocket, &overlapped, &sendBytes, FALSE, NULL);
 		}
-		else
-		{
-			ErrorHandling("WSASend() error");
+		else {
+			ErrorHandling("WSASend error!");
 		}
-	}
 
-	printf("Send data size: %d \n", sendBytes);
-	WSACloseEvent(evObj);
-	closesocket(hSocket);
-	WSACleanup();
-	return 0;
+		printf("Send data size: %d \n", sendBytes);
+		WSACloseEvent(evObj);
+		closesocket(hSocket);
+		WSACleanup();
+		return 0;
+	}
 }
 
 void ErrorHandling(char* msg) {
